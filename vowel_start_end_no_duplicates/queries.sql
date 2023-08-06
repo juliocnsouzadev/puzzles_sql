b6cf91734b6836e57eb070d8be4d7de089ef252f
juliocnsouzadev <juliocnsouzadev@gmail.com>
2023-08-06 12:08:28 +0100

    refactor: Vowel Start End No Duplicates (description and query) to its own folder.

---
+++ vowel_start_end_no_duplicates/queries.sql
@@ -0,0 +1,10 @@
+
+ /*
+ Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
+ as both their first and last characters. Your result cannot contain duplicates.
+ */
+SELECT DISTINCT CITY FROM STATION 
+WHERE 
+SUBSTRING(CITY,1,1) IN('a','e','i','o','u')
+AND
+SUBSTRING(CITY,LENGTH(CITY),1) IN('a','e','i','o','u')
