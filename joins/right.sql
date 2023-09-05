68d63f6772868a4e9e217e8b8e5ad188bf669bd7
juliocnsouzadev <juliocnsouzadev@gmail.com>
2023-09-05 16:29:56 +0100

    feat: right joins

---
+++ joins/right.sql
@@ -0,0 +1,8 @@
+SELECT cities.name AS city, urbanarea_pop, countries.name AS country,
+       indep_year, languages.name AS language, percent
+FROM languages
+  RIGHT JOIN countries
+    ON languages.code = countries.code
+  RIGHT JOIN cities
+    ON countries.code = cities.country_code
+ORDER BY city, language;
