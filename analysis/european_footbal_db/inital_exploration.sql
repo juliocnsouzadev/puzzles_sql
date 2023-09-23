565c0bb180f17c0e78c3796784126140f21842b1
juliocnsouzadev <juliocnsouzadev@gmail.com>
2023-09-23 08:37:24 +0100

    feat: exploring with case clauses

--- analysis/european_footbal_db/inital_exploration.sql
+++ analysis/european_footbal_db/inital_exploration.sql
@@ -22,3 +22,37 @@ FROM Match
 INNER JOIN Team
 ON team_api_id = home_team_api_id
 WHERE team_long_name LIKE '%Barcelona%';
+
+SELECT 
+	date,
+	CASE 
+                WHEN home_team_api_id = 8634 THEN 'FC Barcelona' 
+                ELSE 'Real Madrid CF' 
+        END as home,
+	CASE 
+                WHEN away_team_api_id = 8634 THEN 'FC Barcelona' 
+                ELSE 'Real Madrid CF' 
+        END as away,
+	CASE 
+                WHEN home_team_goal > away_team_goal AND home_team_api_id = 8634 THEN 'Barcelona win!'
+                WHEN home_team_goal > away_team_goal AND home_team_api_id = 8633 THEN 'Real Madrid win!'
+                WHEN home_team_goal < away_team_goal AND away_team_api_id = 8634 THEN 'Barcelona win!'
+                WHEN home_team_goal < away_team_goal AND away_team_api_id = 8633 THEN 'Real Madrid win!'
+                ELSE 'Tie!' 
+        END AS outcome
+FROM Match
+WHERE (away_team_api_id = 8634 OR home_team_api_id = 8634)
+      AND (away_team_api_id = 8633 OR home_team_api_id = 8633);
+
+
+SELECT 
+	season,
+    date,
+	home_goal,
+	away_goal
+FROM matches_italy
+WHERE 
+	CASE 
+		WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win' 
+		END IS NOT NULL;
