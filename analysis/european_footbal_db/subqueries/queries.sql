0a8edbe3eb8a5167bce67e30865806f5630eeffa
juliocnsouzadev <juliocnsouzadev@gmail.com>
2023-12-17 12:17:08 +0000

    feat: filter subquery result with another query

--- analysis/european_footbal_db/subqueries/queries.sql
+++ analysis/european_footbal_db/subqueries/queries.sql
@@ -113,3 +113,21 @@ SELECT
 FROM match AS m
 WHERE season = '2012/2013'
 GROUP BY stage;
+
+-- filter using subqueries
+SELECT 
+	-- Select the stage and average goals from the subquery
+	stage,
+	ROUND(s.avg_goals,2) AS avg_goals
+FROM 
+	-- Select the stage and average goals in 2012/2013
+	(SELECT
+		 stage,
+         AVG(home_goal + away_goal) AS avg_goals
+	 FROM match
+	 WHERE season = '2012/2013'
+	 GROUP BY stage) AS s
+WHERE 
+	-- Filter the main query using the subquery
+	s.avg_goals > (SELECT AVG(home_goal + away_goal) 
+                    FROM match WHERE season = '2012/2013');
